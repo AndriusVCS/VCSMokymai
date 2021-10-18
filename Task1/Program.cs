0d43b4043ff6932a61fb9f2f4bf03104f3502220
Andrius <andriusjvcs@gmail.com>
2021-10-18 09:06:52 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        LabaiSudetingasKompresijosWorkeris manoWorkeris = new LabaiSudetingasKompresijosWorkeris();
+
+        private static void Main(string[] args)
+        {
+            using(FileStream fs = new FileStream("c:/test/test.exe", FileMode.Open))
+            {
+
+            }
+
+
+
+            using (ManoKlase manoKlase = new ManoKlase())
+            {
+                manoKlase.ToString();
+            }
+
+        }
+
+
+        public class ManoKlase : IDisposable
+        {
+            string[] labaiDidelisMasyvas = new string[1000];
+
+            public void Dispose()
+            {
+                labaiDidelisMasyvas = null;
+            }
+        }
+    }
+}
